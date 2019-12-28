b4242d9feec72815e9db270811e76f27a85e69a7
Nahuel Valencia <nahuel.valencia21@gmail.com>
2019-12-28 14:27:35 -0300

    User UI

--- app/src/main/java/com/example/rest_api_retrofit_android/UserActivity.kt
+++ app/src/main/java/com/example/rest_api_retrofit_android/UserActivity.kt
@@ -2,16 +2,43 @@ package com.example.rest_api_retrofit_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class UserActivity : AppCompatActivity() {
+
+    lateinit var avatar: ImageView
+    lateinit var userName: TextView
+    lateinit var followers: TextView
+    lateinit var following: TextView
+    lateinit var logIn: TextView
+    lateinit var email: TextView
+    lateinit var ownedRepos: Button
+
+    lateinit var extras: Bundle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
 
         val actionBar = supportActionBar
-        actionBar!!.setTitle("User Activity")
+        actionBar!!.title = "User Activity"
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        extras = intent.extras!!
+
+        avatar = findViewById(R.id.user_avatar)
+        userName = findViewById(R.id.username)
+        followers = findViewById(R.id.followers)
+        following = findViewById(R.id.following)
+        email = findViewById(R.id.email)
+        ownedRepos = findViewById(R.id.btn_owned_repos)
+
+        userName.text = extras.getString("user_name")
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
