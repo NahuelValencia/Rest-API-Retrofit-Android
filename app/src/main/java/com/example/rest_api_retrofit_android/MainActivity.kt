7d827af1f81d4cbe9d472416c43610ed41342b09
Nahuel Valencia <nahuel.valencia21@gmail.com>
2019-12-28 12:00:38 -0300

    Screen Splash added

---
+++ app/src/main/java/com/example/rest_api_retrofit_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rest_api_retrofit_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
